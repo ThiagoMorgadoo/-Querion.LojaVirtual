ed62f4b33b2aeea8a339a2957ff80913f31f1098
Thiago Morgado <Thiago-morgado@outlook.com.br>
2018-06-19 15:28:52 -0300

    Add project files.

---
+++ Querion.LojaVirtual.UniTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Querion.LojaVirtual.UniTest
+{
+    [TestClass]
+    public class UnitTestQuiron
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ Querion.lojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Querion.lojaVirtual.Web.MvcApplication" Language="C#" %>
